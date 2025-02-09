172350f0a87aee0db9a9d9f0c83261bfe8065dc3
Henrique <matosh38@gmail.com>
2025-02-09 13:46:37 -0300

    Localizando programa em portugues

--- Adivinhacao/src/advinhacao.c
+++ Adivinhacao/src/advinhacao.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include "random.h"
 #include <stdbool.h>
+#include <locale.h>
 
 
-// FunÃ§Ã£o para verificar se os nÃºmeros sortiados e digitado pelo usuÃ¡rio sÃ£o iguais
-bool compare(int sort, int num){
-    if(sort == num){
-        return true;
+// Valores das teclas tabela ANSI
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_ESC 27
+#define KEY_ENTER 13
+
+// Função para verificar se os números sorteados e digitado pelo usuários são iguais
+void compare(int n, int m){
+    if(n == m){
+        printf("Você acertou!\n");
     }
-    return false;
+    else{
+        if (n>m){
+            printf("O valor sorteado é menor que %d\n", n);
+        } else{
+            printf("O valor sorteado é maior que %d\n", n);
+        }
+        
+    }
 }
+bool run(int n, int m){
+    if(n==m){
+        return false;
+    }
+    return true;
+}
+
 
 
 void showMenu(){
@@ -17,6 +38,18 @@ void showMenu(){
 }
 
 int main(){
-    int i = random();
-    printf("%d", i);
+    setlocale(LC_ALL, "Portuguese");
+    bool Run = true;
+    int n,m;   
+
+    m = Random(1,100);
+    do{
+        printf("Digite um número: ");
+        scanf("%d", &n);
+        compare(n,m);
+        Run = run(n,m);
+    }while (Run);
+  
+    
+    
 }
